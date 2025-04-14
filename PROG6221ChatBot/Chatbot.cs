9098db88d406b53df9f928d8a02b2035e0e7bae0
LucNaude <164498775+LucNaude@users.noreply.github.com>
2025-04-14 07:41:39 +0200

    Add project files.

---
+++ PROG6221ChatBot/Chatbot.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG6221ChatBot
+{
+    class Chatbot
+    {
+            public void Opening()
+            {
+                Console.WriteLine("Hello! How can I help you today?");
+
+                while (true)
+                {
+                    string userInput = Console.ReadLine();
+
+                    string response = BotLines(userInput);
+                    Console.WriteLine(response);
+
+                    if (userInput.ToLower().Contains("bye"))
+                        break;
+                }
+            }
+
+            public string BotLines(string userInput)
+            {
+                userInput = userInput.ToLower();
+
+            if (userInput == "hello" || userInput == "hey" || userInput == "hi")
+                    return "Hello Again! Hopefully, I can I help you today?";
+                else if (userInput == "bye" || userInput == "goodbye" || userInput == "bye bye")
+                    return "Goodbye!";
+                else if (userInput == "What's your purpose")
+                return "The weather is sunny today!";
+                else
+                    return "I'm sorry, I don't understand. Please ask me something else.";
+            }
+
+    }
+
+
+}
---
+++ PROG6221ChatBot/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG6221ChatBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Chatbot bot = new Chatbot();
+                bot.Opening();
+            
+        }
+    }
+}
